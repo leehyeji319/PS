b70d74273595019ce69c725a2b3e740044ed9757
zzambbang <zzambbang@gmail.com>
2021-07-24 02:42:08 +0900

    구현문제 복습

--- P0721/3-4.py
+++ P0721/3-4.py
@@ -16,3 +16,18 @@ for i in data: #공포도를 낮은거부터 하나씩 확인하기
         count = 0 #현재 그룹에 포함된 모험가의 수 초기화
 print(result) # 총 그룹의 수 출력
 
+
+
+# n = int(input())
+# data = list(map(int, input().split))
+# data.sort()
+
+# result = 0
+# count = 0
+
+# for i in data:
+#     count += 1
+#     if count >= i:
+#         result += 1
+#         count = 0
+# print(result)
