878ada934ba4fb45c1d916b1c9cc422a53bec794
zzambbang <zzambbang@gmail.com>
2021-07-23 15:31:02 +0900

    구현 복습하기

--- P0721/4-4.py
+++ P0721/4-4.py
@@ -21,3 +21,21 @@ if value != 0:
 
 #최종 결과 출력(리스트를 문자열로 변환하여 출력한다)
 print(''.join(result))
+
+
+# data = input()
+# result = []
+# value = 0
+
+# for x in data:
+#     if x.isalpha
+#         result.append(x)
+#     else:
+#         value += int(x)
+
+# result.sort()
+
+# if value != 0:
+#     result.append(str(value))
+
+# print(''.join(result))
