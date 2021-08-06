857b0caa117057f1f718159f4f9c647176780386
zzambbang <zzambbang@gmail.com>
2021-08-06 18:06:15 +0900

    dfs, bfs 예제 문제 완료

--- P0806/5-10.py
+++ P0806/5-10.py
@@ -1,5 +1,13 @@
 #DFS/BFS 음료수 얼려먹기
 
+ #N, M을 공백을 기준으로 구분하여 입력 받기 
+n, m = map(int ,input().split())
+    
+#2차원 리스트의 맵 정보 입력받기 
+graph = []
+for i in range(n):
+    graph.append(list(map(int , input())))
+
 #DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
 def dfs(x, y):
     #주어진 범위를 벗어나는 경우에는 즉시 종료
@@ -17,19 +25,11 @@ def dfs(x, y):
         return True
     return False
 
-    #N, M을 공백을 기준으로 구분하여 입력 받기 
-    n, m = map(int ,input().split())
-    
-    #2차원 리스트의 맵 정보 입력받기 
-    graph = []
-    for i in range(n):
-        graph.append(list(map(int , input())))
-
-    #모든 노드(위치)에 대하여 음료수 채우기
-    result = 0
-    for i in range(n):
-        for j in range(m):
-            #현재 위치에서 DFS 수행 
-            if dfs(i, j) == True:
-                result += 1
-    print(result) #정답출력
+#모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        #현재 위치에서 DFS 수행 
+        if dfs(i, j) == True:
+            result += 1
+print(result) #정답출력
