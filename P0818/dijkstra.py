139dcc208372805ffc636e5625151fd5de6ece14
zzambbang <zzambbang@gmail.com>
2021-08-19 15:38:24 +0900

    다익스트라 우선순위 큐

--- P0818/dijkstra.py
+++ P0818/dijkstra.py
@@ -5,14 +5,19 @@ INF = int(1e9) #무한을 의미하는 값으로 10억을 생성
 
 # 노드의 개수, 간선의 개수를 입력받기
 n, m = map(int, input().split())
+
 # 시작 노드 번호를 입력받기
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+
 graph = [[] for i in range(n + 1)]
 # 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
 visited = [False] * (n + 1)
+
 # 최단거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n + 1)
+
+
 
 # 모든 간선 정보를 입력받기 
 for _ in range(m):
@@ -29,6 +34,7 @@ def get_smallest_node():
             min_value = distance[i]
             index = i
     return index
+
 
 def dijkstra(start):
     # 시작 노드에 대해서 초기화
