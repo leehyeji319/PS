8db965eee56c709bcca77724acc8093e69cde5c1
zzambbang <zzambbang@gmail.com>
2021-08-25 16:45:20 +0900

    크루스칼 알고리즘 - 최소신장트리 오류 수정

--- P0825/10-5.py
+++ P0825/10-5.py
@@ -4,7 +4,7 @@
 def find_parent(parent, x):
     # 루트 노드를 찾을 때까지 재귀 호출
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x]):
+        parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
 # 두 원소가 속한 집합을 합치기
@@ -46,4 +46,3 @@ for edge in edges:
         result += cost
 
 print(result)
-
