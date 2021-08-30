b79707be488020fcdf58886e25adf49543817c74
zzambbang <zzambbang@gmail.com>
2021-08-30 19:08:15 +0900

    스택수열..^^

---
+++ 백준 /스택, 큐/스택수열.py
@@ -0,0 +1,25 @@
+import sys
+N = int(input())
+add = 0 # 추가 대야하는 거
+op = '' # 연산자 + - 
+stack = []
+
+for i in range(N):
+    a = int(input())
+    if a > add:
+        while a > add:
+           add += 1
+           stack.append(add)
+           op += '+'
+        stack.pop()
+        op += '-'
+    else:
+        if stack[-1] != a:
+            print("NO")
+            sys.exit(0)
+        else:
+            stack.pop()
+            op += '-'
+
+for i in op:
+    print(i)
