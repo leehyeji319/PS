9f66e729b21c21fd7c529b8db59c475a636952ac
zzambbang <zzambbang@gmail.com>
2021-09-10 00:41:21 +0900

    프로그래머스 dfs,bfs 타겟넘버

---
+++ 프로그래머스/DFS,BFS/타켓 넘버.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43165/solution_groups?language=python3&type=my
+
+def solution(numbers, target):
+    parents  = [0]
+    for number in numbers:
+        child = []
+        for parent in parents:
+            child.append(parent + number)
+            child.append(parent - number)
+
+        parents = child
+    return parents.count(target)
