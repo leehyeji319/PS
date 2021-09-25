2d26c4e25587f4717a7f3a35a3924e748ac8ed4d
zzambbang <zzambbang@gmail.com>
2021-09-25 20:28:33 +0900

    백준 단계별

--- 백준 /example.py
+++ 백준 /example.py
@@ -1,16 +1,8 @@
-# 2884 단계별로 풀어보기 if문
-H, M = map(int, input().split())
+import sys
 
-if M >= 45:
-    M -= 45
-else:
-    H -= 1
-    M += 15 
-
-if H < 0:
-    H += 24
-
-print(H, M)
-
-
-# 백준 단계별
+while 1:
+    try:
+        A, B = map(int, sys.stdin.readline().split())
+        print(A + B)
+    except:
+        break
