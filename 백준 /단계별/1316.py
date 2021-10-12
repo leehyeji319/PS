e33dde227ed60464564061848523b222560e0830
zzambbang <zzambbang@gmail.com>
2021-10-12 22:53:05 +0900

    1316 문자열

--- 백준 /단계별/1316.py
+++ 백준 /단계별/1316.py
@@ -1,1 +1,13 @@
 # https://www.acmicpc.net/problem/1316
+n = int(input())
+error = 0
+for i in range(n):
+    word=input()
+    for j in range(len(word) - 1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            error += 1
+            break
+print(n - error)
+
