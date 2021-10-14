7fda678138225ddd5fde2def5da95b90d8c1557d
zzambbang <zzambbang@gmail.com>
2021-10-14 16:14:44 +0900

    2차원 배열 열정렬 인데 같을경우 첫번째 경우 설정

--- 백준 /단계별/11651.py
+++ 백준 /단계별/11651.py
@@ -5,7 +5,7 @@ l = []
 for _ in range(n):
     l.append(list(map(int, sys.stdin.readline().split())))
 
-l.sort(key=lambda x:x[1])  # 중요해중요 ~!~
+l.sort(key=lambda x: (x[1], x[0]))  # 두번째값이 같을 경우 첫번째 값을 기준으로 오름차순 
 # 2차원 배열에서 열을 기준으로 오름차순 할때 람다식 이용
 # arr.sort(key=lambda x:x[1])
 
