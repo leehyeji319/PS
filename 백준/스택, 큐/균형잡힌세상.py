2fe13c13a6ebf99346a0e5b18a812a02ed4dc934
zzambbang <zzambbang@gmail.com>
2021-10-18 15:19:47 +0900

    스택 균형잡힌세상

---
+++ 백준/스택, 큐/균형잡힌세상.py
@@ -0,0 +1,24 @@
+while True:
+    str = input()
+    stack = []
+    if str == ".":
+        break
+    for s in str:
+        if s == '(' or s =='[':
+            stack.append(s)
+        elif s == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(s)
+                break
+        elif s == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(s)
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
