abce588ca108dd4cb405ceeefe969a288555a081
zzambbang <zzambbang@gmail.com>
2021-10-19 17:18:39 +0900

    수학적 사고력

---
+++ 백준/단계별/1193.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+line = 0
+max_num = 0
+while n > max_num:
+    line += 1
+    max_num += line
+
+gap = max_num - n
+
+if line % 2 == 0:
+    top = line - gap
+    under = gap + 1
+else:
+    top = gap + 1
+    under = line - gap 
+print(f'{top}/{under}')
