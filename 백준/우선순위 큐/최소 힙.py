336ebbe53179ab4c24e371e3c0f2c61b7599a56b
zzambbang <zzambbang@gmail.com>
2021-12-19 19:07:44 +0900

    최소힙

--- 백준/우선순위 큐/최소 힙.py
+++ 백준/우선순위 큐/최소 힙.py
@@ -1,15 +1,15 @@
-import heapq
 import sys
+import heapq
 
 n = int(input())
 h = []
+
 for i in range(n):
     a = int(sys.stdin.readline())
     if a != 0:
-        heapq.heappush(h, (-a))
+        heapq.heappush(h, (a))
     else:
         if len(h) == 0:
             print(0)
         else:
-            print(-1 * heapq.heappop(h))
-            
+            print(heapq.heappop(h))
