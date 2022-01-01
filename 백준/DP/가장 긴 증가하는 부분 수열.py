88ad757b90f302190f04b3b7f02fa9fe70c3a863
zzambbang <zzambbang@gmail.com>
2022-01-01 15:26:05 +0900

    dp 가장 긴 증가하는 부분 수열

---
+++ 백준/DP/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,11 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+dp = [1] * n # dp 테이블 초기화
+
+# 가장 긴 증가하는 부분수열 알고리즘 수행
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
