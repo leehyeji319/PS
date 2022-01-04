86417a4963114813b4c0468b2136fe2dd49af676
zzambbang <zzambbang@gmail.com>
2022-01-04 19:50:55 +0900

    소수판정

--- 백준/BOJ/2021은 무엇이 특별할까.py
+++ 백준/BOJ/2021은 무엇이 특별할까.py
@@ -1,1 +1,15 @@
 # https://www.acmicpc.net/problem/24039
+num = 1000
+a = [False, False] + [True] * (num - 1)
+primes = []
+for i in range(2, num + 1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2*i, num+1, i):
+            a[j] = False
+n = int(input())
+for i in range(0, len(primes) - 1):
+    if primes[i] * primes[i + 1] > n:
+        print(primes[i] * primes[i + 1])
+        exit(0)
+    
