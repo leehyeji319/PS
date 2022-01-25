e51e9395d8d049bba92f5f93666e996783eef4e8
Hyeji Lee <zzambbang@gmail.com>
2022-01-25 15:52:18 +0900

    프로그래머스 python

---
+++ 프로그래머스/해시/전화번호 목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    answer = True
+    num = sorted(phone_book)
+    for i in range(len(phone_book) - 1):
+        if num[i] == num[i + 1][:len(num[i])]:
+            answer = False
+    return answer
+
+print(solution(["123","456","789"]))
