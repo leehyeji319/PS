485718f9ff8887c81c4de5cd70a51eb16e1eb597
Hyeji Lee <zzambbang@gmail.com>
2022-02-24 21:31:59 +0900

    bfs 미로 탐색

---
+++ 백준/DFS BFS/미로 탐색.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input())))
+    
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            # 현재 위치에서 네방향으로 위치 확인
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            #벽이면 무시
+            if graph[nx][ny] == 0:
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    
+    #최단거리 반환
+    return graph[n - 1][m - 1]
+
+print(bfs(0,0))
