660674629506b0be17ad9376759e1cde68add544
Hyeji Lee <zzambbang@gmail.com>
2022-03-23 23:32:45 +0900

    큰 수 만들기

---
+++ 프로그래머스/그리디/큰 수 만들기.py
@@ -0,0 +1,25 @@
+def solution(number, k):
+    answer = []
+    
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+            
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+    
+    if k > 0:
+        for i in range(k):
+            answer.pop()
+            
+    print(answer)
+
+    return ''.join(answer)
+    
+print(solution("4177252841", 4))
