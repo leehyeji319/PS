93ea9de0e64d31c3168d45b0deb343205e459d9e
Hyeji Lee <zzambbang@gmail.com>
2022-03-27 18:13:19 +0900

    백준 백트래킹

---
+++ 백준/백트래킹/N과M1.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+solve = []
+
+def dfs(depth):
+    if depth == m:
+        for i in solve:
+            print(i, end = '')
+            print()
+            return 
+    for i in range(1, n + 1):
+        if i not in solve:
+            solve.append(i)
+            dfs(depth + 1)
+            solve.pop()
+dfs(0)
