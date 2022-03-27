b1032374ed75ad518a2351d64e52ac759c2dee08
Hyeji Lee <zzambbang@gmail.com>
2022-03-27 18:20:45 +0900

    백준 백트래킹 출력 오류 수정

--- 백준/백트래킹/N과M1.py
+++ 백준/백트래킹/N과M1.py
@@ -4,9 +4,9 @@ solve = []
 def dfs(depth):
     if depth == m:
         for i in solve:
-            print(i, end = '')
-            print()
-            return 
+            print(i, end = ' ')
+        print()
+        return 
     for i in range(1, n + 1):
         if i not in solve:
             solve.append(i)
