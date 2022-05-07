9e47d003effe5ba989d9ba797b8cbcf54d2adcce
Hyeji Lee <zzambbang@gmail.com>
2022-05-07 15:17:48 +0900

    feat: 피어세션3주차 더 맵게 해시

--- 프로그래머스/피어리뷰/더 맵게.py
+++ 프로그래머스/피어리뷰/더 맵게.py
@@ -4,19 +4,18 @@ def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
     
-    while len(scoville) >= 2: 
-        # 최솟값 가져오기
+    while len(scoville) >= 2:
         min_1 = heapq.heappop(scoville)
         
-        if min_1 >= K: #최솟값이 k보다 크면 종료
+        if min_1 >= K:
             return answer
-        else: # 두 번째로 작은 값 가져와서 합친 값을 힙에넣기
+        else:
             min_2 = heapq.heappop(scoville)
-            heapq.heappush(scoville, min_1 + (min_2 * 2 ))
+            heapq.heappush(scoville, min_1 + (min_2 * 2))
             answer += 1
     if scoville[0] >= K:
         return answer
     else:
         return -1
-    
+
 print(solution([1, 2, 3, 9, 10, 12], 7))
