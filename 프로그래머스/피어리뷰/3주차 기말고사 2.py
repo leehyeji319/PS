fc92a2e8aeecde253789fabda79407cd0c2ea95c
Hyeji Lee <zzambbang@gmail.com>
2022-05-13 19:23:23 +0900

    3주차 기말고사 2번문제

--- 프로그래머스/피어리뷰/3주차 기말고사 2.py
+++ 프로그래머스/피어리뷰/3주차 기말고사 2.py
@@ -1,5 +1,47 @@
 #레벨3 : 선후수 수강
 
+from collections import defaultdict
+
 def solution(s1, s2, k):
     answer = []
+
+    check = defaultdict(list)
+    graph = defaultdict(list)
+    for a, b in zip(s1,s2):
+        check[b].append(a)
+        graph[a].append(b)
+
+    stack = [k]
+    subjects = []
+    while stack:
+        subject_list = stack.pop()
+        for subject in check[subject_list]:
+            if check[subject] == []:
+                subjects.append(subject)
+            else:
+                stack.append(subject)
+    subjects = list(set(subjects))
+
+    finished = False
+
+    while subjects and not finished:
+        subjects.sort()
+        
+        for subject in subjects:
+            if subject in answer:
+                continue
+            if check[subject] == []:
+                subjects.remove(subject)
+                answer.append(subject)
+                if subject == k:
+                    finished = True
+                    break
+                for s in graph[subject]:
+                    if subject in check[s]:
+                        check[s].remove(subject)
+                    subjects.append(s)
+                break
+    
     return answer
+
+print(solution(["A", "E", "B", "D", "B", "H", "F", "H", "C"], ["G", "C", "G", "F", "J", "E", "B", "F", "B"], "B"))
