d89b6902ca7aafbc83e52bf760397ee7b21dcf57
zzambbang <zzambbang@gmail.com>
2022-05-28 13:30:10 +0900

    feat: 최장경로

--- 삼성ExpertAcademy/최장경로.py
+++ 삼성ExpertAcademy/최장경로.py
@@ -1,8 +1,30 @@
 T = int(input())
+
+
+def dfs(graph, v, visited, ans):
+    global answer
+    visited[v] = True
+    ans += 1
+    #최대값 갱신
+    if answer < ans:
+        answer = ans
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited, ans)
+    visited[v] = False
+
+
 for t in range(1, T + 1):
-    N, M = map(int, input().split())
-    graph = [[] for _ in range(N + 1)]
-    for i in range(M):
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n + 1)]
+    visited = [False] * (n + 1)
+    answer = 0
+    for _ in range(m):
         a, b = map(int, input().split())
         graph[a].append(b)
-    print(graph)
+        graph[b].append(a)
+    #시작 노드를 다르게 해서 최대 길이 찾기
+    for i in range(n):
+        dfs(graph, i, visited, 0)
+    print(f"#{t} {answer}")
