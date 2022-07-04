3cd9726737ce301a79d60ca0efb80a2f38ada7a5
Hyeji Lee <zzambbang@gmail.com>
2022-07-04 23:12:36 +0900

    feat(SWEA): 문제해결

--- 삼성ExpertAcademy/파리퇴치.py
+++ 삼성ExpertAcademy/파리퇴치.py
@@ -2,4 +2,14 @@ T = int(input())
 
 for t in range(1, T + 1):
     N, M = map(int, input().split())
-    graph = list()
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    fly = 0
+    for i in range(N - M + 1):
+        for j in range(N - M + 1):
+            tmp = 0
+            for a in range(i, i + M):
+                for b in range(j, j + M):
+                    tmp += graph[a][b]
+            if tmp > fly:
+                fly = tmp
+    print(f"#{t} {fly}")
