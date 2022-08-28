c322543e4be58147029720d9ae69973dc3c1d197
zzambbang <zzambbang@gmail.com>
2022-08-28 14:54:23 +0900

    swea:: 낚시터 자리잡기, 헌터 문제해결

--- 삼성ExpertAcademy/낚시터 자리잡기.py
+++ 삼성ExpertAcademy/낚시터 자리잡기.py
@@ -3,13 +3,13 @@ from itertools import permutations
 from collections import defaultdict
 
 input = sys.stdin.readline
-# 그리디? 브루트 포스 ?
+# 브루트 포스 ?
 T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
-    graph = [[0] * N for _ in range(2)]
     old_door_people_dict = defaultdict(list)
     people_len = 0
+    #출입문과 낚시꾼 정보 입력받기(딕셔너리)
     for _ in range(3):
         a, b = map(int, input().split())
         old_door_people_dict[a - 1] = [i for i in range(people_len + 1, b + people_len + 1)]  # 딕셔너리로 저장
@@ -26,9 +26,12 @@ for tc in range(1, T + 1):
     able_people = [0] * people_len
     for i in range(people_len):
         able_people[i] = i + 1
+
+    #순열을 위한 배열 생성
     min_value = int(1e9)
     able_people += [0] * (N - people_len)
 
+    #순열 돌리기
     for perm in permutations(able_people):
         distance = 0
         perm = list(perm)
