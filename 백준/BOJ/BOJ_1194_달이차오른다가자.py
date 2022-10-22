7716f0d68b777d5cbb991e7ef9c75b8da0bb5bbd
zzambbang <zzambbang@gmail.com>
2022-10-22 15:37:33 +0900

    refactor 파일 정리

--- 백준/BOJ/BOJ_1194_달이차오른다가자.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-N, M = map(int, input().split())
-
-board = [list(input().rstrip()) for _ in range(N)]
-
-keys = ['a', 'b', 'c', 'd', 'e', 'f']
-doors = ['A', 'B', 'C', 'D', 'E', 'F']
