60a644a9ae561129f168dbcb1de342f105a9f1bc
leehyeji <zzambbang@gmail.com>
2023-01-06 09:43:58 +0900

    boj:: 2644_촌수계산 문제해결

--- 백준/DFS BFS/BOJ_2644_촌수계산_DFS.py
+++ 백준/DFS BFS/BOJ_2644_촌수계산_DFS.py
@@ -1,34 +1,37 @@
 import sys
+
 input = sys.stdin.readline
 
-
-N = int(input()) # 노드 수
+N = int(input())  # 노드 수
 t1, t2 = map(int, input().split())
-L = int(input()) # 간선 수
+L = int(input())  # 간선 수
 
 graph = [[] for _ in range(N + 1)]
 
 visited = [False] * (N + 1)
-
+result = []
 for _ in range(L):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-ans = 0
 
-
-def dfs(graph, v, visited):
+def dfs(v, ans):
+    ans += 1
     if v == t2:
+        result.append(ans)
         return
 
     visited[v] = True
-    global ans
+
     for i in graph[v]:
         if not visited[i]:
-            ans += 1
-            dfs(graph, i, visited)
+            dfs(i, ans)
 
-dfs(graph, t1, visited)
 
-print(ans)
+dfs(t1, 0)
+
+if len(result) == 0:
+    print(-1)
+else:
+    print(result[0] - 1)
