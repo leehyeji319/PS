75fffa400857c1a55583a6252f10125572688678
Hyeji Lee <zzambbang@gmail.com>
2023-04-20 12:49:56 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if len(cards1) > 0 and g == cards1[0]:
+            cards1 = cards1[1:]
+        elif len(cards2) > 0 and g == cards2[0]:
+            cards2 = cards2[1:]
+        else:
+            return 'No'
+    return 'Yes'
